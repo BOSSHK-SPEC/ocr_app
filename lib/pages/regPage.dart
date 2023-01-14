db861d8f9a3808f83c5e41a843f3819ba8bca1ed
BOSSHK-SPEC <yashwanthhk2000@gmail.com>
2023-01-14 13:36:38 +0530

    regPage

---
+++ lib/pages/regPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class regPage extends StatefulWidget {
+  const regPage({Key? key}) : super(key: key);
+
+  @override
+  State<regPage> createState() => _regPageState();
+}
+
+class _regPageState extends State<regPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
